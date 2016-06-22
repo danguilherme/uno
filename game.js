8c108344f1f087935774e694e033008c5af8913a
Guilherme Ventura <guilhermeventura2@gmail.com>
2016-06-22 10:00:14 -0300

    Events for game class

--- game.js
+++ game.js
@@ -1,1 +1,20 @@
-var card = require('./card');
+"use strict";
+
+const util = require('util');
+const EventEmitter = require('events').EventEmitter;
+
+const card = require('./card');
+
+const game = function(players) {
+    // extends EventEmitter
+    // events:
+    // - start (players)
+    // - card-play (card)
+    // - uno (player)
+    // - end (winner)
+
+    let instance = {};
+    util.inherits(game, EventEmitter);
+
+    return;
+};
