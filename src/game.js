29ca840acf06dd0df970c00ec72db111c8324b6e
Guilherme Ventura <guilhermeventura2@gmail.com>
2018-06-13 09:51:50 -0300

    Fix `args2arry is not defined`

--- src/game.js
+++ src/game.js
@@ -308,7 +308,6 @@ const game = function (playerNames) {
   }
 
   function emit(eventName) {
-    const args = args2array(arguments);
     let result = false;
     try {
       result = instance.emit.apply(instance, arguments);
