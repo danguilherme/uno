f6112a20f9cac438fc056a2df4796340ba5c74ab
Guilherme Ventura <guilhermeventura2@gmail.com>
2018-06-13 21:29:28 -0300

    Configure correct TypeScript structure
    - Follow structure from github.com/Microsoft/TypeScript-Node-Starter

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./card/card"));
 __export(require("./card/value"));
+//# sourceMappingURL=index.js.map
