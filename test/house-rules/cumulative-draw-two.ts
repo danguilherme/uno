1138e42d2cdb501de29e5bfefe6333c805eacfbe
Guilherme Ventura <guilhermeventura2@gmail.com>
2018-06-15 14:02:21 -0300

    Test if cumulative draw two doesn't mess with default game flow

--- test/house-rules/cumulative-draw-two.ts
+++ test/house-rules/cumulative-draw-two.ts
@@ -60,4 +60,34 @@ describe('House Rule: Cumulative draw two', function() {
     expect(curr.hand.length).toBe(6);
     expect(game.currentPlayer.name).not.toBe(curr.name);
   });
+
+  it('should not interfere if no DRAW TWO is played', () => {
+    let curr = game.currentPlayer;
+    const discardedCard = game.discardedCard;
+    const reverse = new Card(Values.REVERSE, discardedCard.color);
+
+    curr.hand = [reverse, reverse];
+
+    expect(() => game.play(reverse)).not.toThrow();
+
+    // add reverse to new player hand
+    curr = game.currentPlayer;
+    curr.hand = [reverse, reverse];
+
+    // cannot pass before playing
+    expect(() => game.pass()).toThrow();
+    // can play any matching
+    expect(() => game.play(reverse)).not.toThrow();
+    expect(curr.hand).toHaveLength(1);
+    // lost his turn
+    expect(game.currentPlayer.name).not.toBe(curr.name);
+
+    // add reverse to new player hand
+    curr = game.currentPlayer;
+    curr.hand = [reverse, reverse];
+
+    // can draw
+    expect(() => game.draw()).not.toThrow();
+    expect(curr.hand).toHaveLength(3);
+  });
 });
