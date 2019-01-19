0ef603cadcf8c9ac7d99ed900f724ca50dc1ee13
Guilherme Ventura <guilhermeventura2@gmail.com>
2019-01-19 22:15:57 +0100

    fix: validate color and value input for cards
    - fix Card.toString() function to return the names

--- src/card/values.ts
+++ src/card/values.ts
@@ -17,3 +17,37 @@ export enum Values {
   WILD = 13,
   WILD_DRAW_FOUR = 14,
 }
+
+export namespace Values {
+  /**
+   * Returns true if `value` is {@link Values.WILD} or {@link Values.WILD_DRAW_FOUR}.
+   */
+  export function isWild(value: Values) {
+    return value === Values.WILD || value === Values.WILD_DRAW_FOUR;
+  }
+
+  /**
+   * Runtime type checking
+   */
+  export function isValidValue(value: Values) {
+    return values.indexOf(value) !== -1;
+  }
+
+  export const values = [
+    Values.ZERO,
+    Values.ONE,
+    Values.TWO,
+    Values.THREE,
+    Values.FOUR,
+    Values.FIVE,
+    Values.SIX,
+    Values.SEVEN,
+    Values.EIGHT,
+    Values.NINE,
+    Values.DRAW_TWO,
+    Values.REVERSE,
+    Values.SKIP,
+    Values.WILD,
+    Values.WILD_DRAW_FOUR,
+  ];
+}
